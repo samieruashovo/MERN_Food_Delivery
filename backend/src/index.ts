5e5a5177cbc06b832af9fe1a222ad9a29dbec072
samieruashovo <samieruashovo@gmail.com>
2024-06-23 13:40:31 +0600

    1st

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string).then(() => {
+  console.log("Connected to MongoDB");
+});
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hello World" });
+});
+
+app.listen(7000, () => {
+  console.log("Server is running on port 7000");
+});
